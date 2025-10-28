13f352c68e35e72f129ceb56e820ac6d60d8c36b
akiyoshi <pasoconnfast@gmail.com>
2025-10-28 09:29:46 +0900

    Update supabaseClient.js

--- public/lib/supabaseClient.js
+++ public/lib/supabaseClient.js
@@ -1,1 +1,5 @@
-a
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
