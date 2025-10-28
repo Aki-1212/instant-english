4c77bef586ffebd65a6b6b7a2d44e64e51b7db92
akiyoshi <pasoconnfast@gmail.com>
2025-10-28 09:34:41 +0900

    Update supabaseClient.js

--- public/lib/supabaseClient.js
+++ public/lib/supabaseClient.js
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = 'https://jtznsuhkvjukqoltncjr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0em5zdWhrdmp1a3FvbHRuY2pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1Mzg0NzAsImV4cCI6MjA3NzExNDQ3MH0.mu6vPCOI-8rkIpuSWB5XCH_aAL_Ji_190NgTh4lZY88' // Supabase ダッシュボードの anon key
 export const supabase = createClient(supabaseUrl, supabaseKey)
